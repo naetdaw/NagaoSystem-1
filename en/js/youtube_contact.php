d38b94b13ea148bb5909e10283989cc899bf942e
naetdaw <41181845+naetdaw@users.noreply.github.com>
2018-07-17 16:56:54 +0700

    Add files via upload

---
+++ en/js/youtube_contact.php
@@ -0,0 +1,266 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="robots" content="all,follow">
+    <meta name="googlebot" content="index,follow,snippet,archive">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Universal - All In 1 Template</title>
+
+    <meta name="keywords" content="">
+
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,500,700,800' rel='stylesheet' type='text/css'>
+
+    <!-- Bootstrap and Font Awesome css -->
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+
+    <!-- Css animations  -->
+    <link href="css/animate.css" rel="stylesheet">
+
+    <!-- Theme stylesheet, if possible do not edit this stylesheet -->
+    <link href="css/style.default.css" rel="stylesheet" id="theme-stylesheet">
+
+    <!-- Custom stylesheet - for your changes -->
+    <link href="css/custom.css" rel="stylesheet">
+
+    <!-- Responsivity for older IE -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+
+    <!-- Favicon and apple touch icons-->
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+    <link rel="apple-touch-icon" href="img/apple-touch-icon.png" />
+    <link rel="apple-touch-icon" sizes="57x57" href="img/apple-touch-icon-57x57.png" />
+    <link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-72x72.png" />
+    <link rel="apple-touch-icon" sizes="76x76" href="img/apple-touch-icon-76x76.png" />
+    <link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-114x114.png" />
+    <link rel="apple-touch-icon" sizes="120x120" href="img/apple-touch-icon-120x120.png" />
+    <link rel="apple-touch-icon" sizes="144x144" href="img/apple-touch-icon-144x144.png" />
+    <link rel="apple-touch-icon" sizes="152x152" href="img/apple-touch-icon-152x152.png" />
+
+    <!-- owl carousel css -->
+
+    <link href="css/owl.carousel.css" rel="stylesheet">
+    <link href="css/owl.theme.css" rel="stylesheet">
+
+</head>
+
+<body>
+  <?php
+    include 'header_index.php';
+    ?>
+        <div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-labelledby="Login" aria-hidden="true">
+            <div class="modal-dialog modal-sm">
+
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                        <h4 class="modal-title" id="Login">Customer login</h4>
+                    </div>
+                    <div class="modal-body">
+                        <form action="customer-orders.html" method="post">
+                            <div class="form-group">
+                                <input type="text" class="form-control" id="email_modal" placeholder="email">
+                            </div>
+                            <div class="form-group">
+                                <input type="password" class="form-control" id="password_modal" placeholder="password">
+                            </div>
+
+                            <p class="text-center">
+                                <button class="btn btn-template-main"><i class="fa fa-sign-in"></i> Log in</button>
+                            </p>
+
+                        </form>
+
+                        <p class="text-center text-muted">Not registered yet?</p>
+                        <p class="text-center text-muted"><a href="customer-register.html"><strong>Register now</strong></a>! It is easy and done in 1&nbsp;minute and gives you access to special discounts and much more!</p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- *** LOGIN MODAL END *** -->
+
+        <div id="heading-breadcrumbs">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-7">
+                        <h1 style="color: #FF3333">Youtube Present</h1>
+                    </div>
+                    <div class="col-md-5">
+                        <ul class="breadcrumb">
+                            <li><a href="index.php">Home</a>
+                            </li>
+                            <li style="color: #FF3333">Youtube </li>
+                        </ul>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div id="content">
+            <div class="container" style="background-color: #F0FFF0">
+
+                <section>
+
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="heading text-center">
+                                <h2 style="background-color: #FFEBCD">Present Product</h2>
+                            </div>
+
+                            <p class="lead"><b>NAGAO SYSTEM INC.</b> Is a professional group that solves crushing, mixing and dispersion, which was considered impossible in the past by high-speed rotation utilization of patented 3D ball mill (3D Reactor).
+
+mm or less (Organic / Inorganic) material can be pulverized, mixed and dispersed up to nano, micron (Dry/Wet type) possible.
+
+Furthermore, it is good to mix substances of different gravity and viscosity with unused and evenly mixed (3D Reactor) pellicles and feathers.
+
+We manufacture and sell the equipment + special container.
+
+Please feel the difference of the unknown area high speed 3D motion from the conventional 2D motion.
+High-speed 3D motion can be expected to reduce mixing unevenness and improve milling uniformity.</p>
+                        </div>
+                    </div>
+
+                    <div class="row portfolio">
+
+                        <div class="col-sm-6 col-md-3">
+                            <div class="box-image">
+                                <div class="image">
+                                  <div class="video">
+                                <div class="embed-responsive embed-responsive-4by3">
+                                    <iframe class="embed-responsive-item" src="//www.youtube.com/embed/HyGXTj_D8Co"></iframe>
+                                </div>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        
+                        <div class="col-sm-6 col-md-3">
+                            <div class="box-image">
+                                <div class="image">
+                                  <div class="video">
+                                <div class="embed-responsive embed-responsive-4by3">
+                                    <iframe class="embed-responsive-item" src="//www.youtube.com/embed/d4WAZl4GVJw"></iframe>
+                                </div>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+
+                       
+                        <div class="col-sm-6 col-md-3">
+                            <div class="box-image">
+                                <div class="image">
+                                  <div class="video">
+                                <div class="embed-responsive embed-responsive-4by3">
+                                    <iframe class="embed-responsive-item" src="//www.youtube.com/embed/G52dMHI6R7U"></iframe>
+                                </div>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        
+                        <div class="col-sm-6 col-md-3">
+                            <div class="box-image">
+                                <div class="image">
+                                  <div class="video">
+                                <div class="embed-responsive embed-responsive-4by3">
+                                    <iframe class="embed-responsive-item" src="//www.youtube.com/embed/yNhlphn4eGs"></iframe>
+                                </div>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div class="col-sm-6 col-md-3">
+                            <div class="box-image">
+                                <div class="image">
+                                  <div class="video">
+                                <div class="embed-responsive embed-responsive-4by3">
+                                    <iframe class="embed-responsive-item" src="//www.youtube.com/embed/p87PKIDiPxg"></iframe>
+                                </div>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+
+                       <div class="col-sm-6 col-md-3">
+                            <div class="box-image">
+                                <div class="image">
+                                  <div class="video">
+                                <div class="embed-responsive embed-responsive-4by3">
+                                    <iframe class="embed-responsive-item" src="//www.youtube.com/embed/9_vwDCopA1g"></iframe>
+                                </div>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+
+                     <div class="col-sm-6 col-md-3">
+                            <div class="box-image">
+                                <div class="image">
+                                  <div class="video">
+                                <div class="embed-responsive embed-responsive-4by3">
+                                    <iframe class="embed-responsive-item" src="//www.youtube.com/embed/T3As0uluTwk"></iframe>
+                                </div>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+
+                   
+
+
+                        
+
+                    </div>
+
+                </section>
+
+            </div>
+            <!-- /.container -->
+
+            
+          
+
+
+        <!-- *** FOOTER ***
+_________________________________________________________ -->
+
+       
+<?php
+include 'footer.php';
+?>
+
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script>
+        window.jQuery || document.write('<script src="js/jquery-1.11.0.min.js"><\/script>')
+    </script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+
+    <script src="js/jquery.cookie.js"></script>
+    <script src="js/waypoints.min.js"></script>
+    <script src="js/jquery.counterup.min.js"></script>
+    <script src="js/jquery.parallax-1.1.3.js"></script>
+    <script src="js/front.js"></script>
+
+    
+
+    <!-- owl carousel -->
+    <script src="js/owl.carousel.min.js"></script>
+
+
+
+</body>
+
+</html>
